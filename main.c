c2f39e7794a83d663ee3997fbe9b4e5c83315332
sohail11998 <80307647+sohail11998@users.noreply.github.com>
2021-04-07 20:22:26 +0530

    Relational operator code.
    Equality operator (==) used.

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
 {
-    printf("Hello world!\n");
+    int a,b,c;
+    printf("Enter the two numbers to compare: ");
+    scanf("%d %d",&a,&b);
+    c = a==b;
+    printf("Entered numbers are %d and %d.\n",a,b);
+    printf("Output: %d\n",c);
+    printf("Numbers are equal if the output is '1' and unequal if output is '0'");
     return 0;
 }
-
-//This is an edit
